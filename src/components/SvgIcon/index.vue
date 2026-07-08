321c8662706c6ba15cb8f453d676154adba1d0ea
annopick <veater@qq.com>
2026-07-09 01:31:01 +0800

    feat: setup main.ts entry, permission guard, icons, migrate SvgIcon to Vue3

--- src/components/SvgIcon/index.vue
+++ src/components/SvgIcon/index.vue
@@ -1,48 +1,28 @@
 <template>
-  <div v-if="isExternal" :style="styleExternalIcon" class="svg-external-icon svg-icon" v-on="$listeners" />
-  <svg v-else :class="svgClass" aria-hidden="true" v-on="$listeners">
+  <div v-if="isExt" :style="styleExternalIcon" class="svg-external-icon svg-icon" v-bind="$attrs" />
+  <svg v-else :class="svgClass" aria-hidden="true" v-bind="$attrs">
     <use :xlink:href="iconName" />
   </svg>
 </template>
 
-<script>
-// doc: https://panjiachen.github.io/vue-element-admin-site/feature/component/svg-icon.html#usage
+<script setup lang="ts">
+import { computed } from 'vue'
 import { isExternal } from '@/utils/validate'
 
-export default {
-  name: 'SvgIcon',
-  props: {
-    iconClass: {
-      type: String,
-      required: true
-    },
-    className: {
-      type: String,
-      default: ''
-    }
-  },
-  computed: {
-    isExternal() {
-      return isExternal(this.iconClass)
-    },
-    iconName() {
-      return `#icon-${this.iconClass}`
-    },
-    svgClass() {
-      if (this.className) {
-        return 'svg-icon ' + this.className
-      } else {
-        return 'svg-icon'
-      }
-    },
-    styleExternalIcon() {
-      return {
-        mask: `url(${this.iconClass}) no-repeat 50% 50%`,
-        '-webkit-mask': `url(${this.iconClass}) no-repeat 50% 50%`
-      }
-    }
-  }
-}
+const props = withDefaults(defineProps<{
+  iconClass: string
+  className?: string
+}>(), {
+  className: ''
+})
+
+const isExt = computed(() => isExternal(props.iconClass))
+const iconName = computed(() => `#icon-${props.iconClass}`)
+const svgClass = computed(() => props.className ? `svg-icon ${props.className}` : 'svg-icon')
+const styleExternalIcon = computed(() => ({
+  mask: `url(${props.iconClass}) no-repeat 50% 50%`,
+  '-webkit-mask': `url(${props.iconClass}) no-repeat 50% 50%`
+}))
 </script>
 
 <style scoped>
@@ -53,10 +33,9 @@ export default {
   fill: currentColor;
   overflow: hidden;
 }
-
 .svg-external-icon {
   background-color: currentColor;
-  mask-size: cover!important;
+  mask-size: cover !important;
   display: inline-block;
 }
 </style>
