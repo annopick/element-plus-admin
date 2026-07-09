a0d956bdd6a45041d9571f78af26f938135ea090
annopick <veater@qq.com>
2026-07-09 13:38:56 +0800

    feat(editors): rewrite MarkdownEditor→md-editor-v3, JsonEditor→vue-codemirror

--- src/components/MarkdownEditor/index.vue
+++ src/components/MarkdownEditor/index.vue
@@ -1,118 +1,47 @@
 <template>
-  <div :id="id" />
+  <MdEditor
+    v-model="text"
+    :style="{ height, width }"
+    :toolbars-exclude="excludeToolbars"
+    @on-save="onSave"
+  />
 </template>
 
-<script>
-// deps for editor
-import 'codemirror/lib/codemirror.css' // codemirror
-import 'tui-editor/dist/tui-editor.css' // editor ui
-import 'tui-editor/dist/tui-editor-contents.css' // editor content
+<script setup lang="ts">
+import { ref, watch } from 'vue'
+import { MdEditor } from 'md-editor-v3'
+import type { ToolbarNames } from 'md-editor-v3'
+import 'md-editor-v3/lib/style.css'
 
-import Editor from 'tui-editor'
-import defaultOptions from './default-options'
+const props = withDefaults(defineProps<{
+  modelValue?: string
+  height?: string
+  width?: string
+}>(), {
+  modelValue: '',
+  height: '300px',
+  width: '100%'
+})
 
-export default {
-  name: 'MarkdownEditor',
-  props: {
-    value: {
-      type: String,
-      default: ''
-    },
-    id: {
-      type: String,
-      required: false,
-      default() {
-        return 'markdown-editor-' + +new Date() + ((Math.random() * 1000).toFixed(0) + '')
-      }
-    },
-    options: {
-      type: Object,
-      default() {
-        return defaultOptions
-      }
-    },
-    mode: {
-      type: String,
-      default: 'markdown'
-    },
-    height: {
-      type: String,
-      required: false,
-      default: '300px'
-    },
-    language: {
-      type: String,
-      required: false,
-      default: 'en_US' // https://github.com/nhnent/tui.editor/tree/master/src/js/langs
-    }
-  },
-  data() {
-    return {
-      editor: null
-    }
-  },
-  computed: {
-    editorOptions() {
-      const options = Object.assign({}, defaultOptions, this.options)
-      options.initialEditType = this.mode
-      options.height = this.height
-      options.language = this.language
-      return options
-    }
-  },
-  watch: {
-    value(newValue, preValue) {
-      if (newValue !== preValue && newValue !== this.editor.getValue()) {
-        this.editor.setValue(newValue)
-      }
-    },
-    language(val) {
-      this.destroyEditor()
-      this.initEditor()
-    },
-    height(newValue) {
-      this.editor.height(newValue)
-    },
-    mode(newValue) {
-      this.editor.changeMode(newValue)
-    }
-  },
-  mounted() {
-    this.initEditor()
-  },
-  destroyed() {
-    this.destroyEditor()
-  },
-  methods: {
-    initEditor() {
-      this.editor = new Editor({
-        el: document.getElementById(this.id),
-        ...this.editorOptions
-      })
-      if (this.value) {
-        this.editor.setValue(this.value)
-      }
-      this.editor.on('change', () => {
-        this.$emit('input', this.editor.getValue())
-      })
-    },
-    destroyEditor() {
-      if (!this.editor) return
-      this.editor.off('change')
-      this.editor.remove()
-    },
-    setValue(value) {
-      this.editor.setValue(value)
-    },
-    getValue() {
-      return this.editor.getValue()
-    },
-    setHtml(value) {
-      this.editor.setHtml(value)
-    },
-    getHtml() {
-      return this.editor.getHtml()
-    }
-  }
+const emit = defineEmits<{
+  (e: 'update:modelValue', val: string): void
+  (e: 'change', val: string): void
+  (e: 'submit', val: string): void
+}>()
+
+const text = ref(props.modelValue)
+
+watch(text, (val) => {
+  emit('update:modelValue', val)
+  emit('change', val)
+})
+
+watch(() => props.modelValue, (val) => {
+  if (val !== text.value) text.value = val
+})
+
+const excludeToolbars: ToolbarNames[] = ['github', 'save']
+function onSave(val: string) {
+  emit('submit', val)
 }
 </script>
