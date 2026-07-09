876798296ae4763a7ff0b0375f10c9b0263699cd
annopick <veater@qq.com>
2026-07-09 11:01:00 +0800

    feat: migrate utils/permission + error-log to TS, implement changeRoles + errorHandler

--- src/main.ts
+++ src/main.ts
@@ -14,6 +14,7 @@ import router from './router'
 import store from './store'
 import { setupIcons } from './icons'
 import { setupDirectives } from './directive'
+import { setupErrorLog } from './utils/error-log'
 
 import './permission'
 
@@ -28,6 +29,7 @@ setupIcons(app)
 setupDirectives(app)
 
 app.use(store)
+setupErrorLog(app)
 app.use(router)
 app.use(ElementPlus, {
   locale: zhCn,
